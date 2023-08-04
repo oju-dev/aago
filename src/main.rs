0d6306c7a710523607ecd0e98e33ba2f691687b6
Vitor Roque <vitor.roquep@gmail.com>
2023-08-04 11:56:04 -0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use aago::trace::{List, Pathtrace, Trace};
+use actix_web::{post, web, App, HttpServer};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct Response {
+    result: String,
+}
+
+#[post("/trace")]
+async fn trace(data: web::Data<RefCell<List>>, trace: web::Json<Trace>) -> web::Json<Response> {
+    let mut mutable_list = data.borrow().pathtraces;
+    let tracing = trace.0;
+    add_to_data(&mut mutable_list, Box::new(tracing));
+
+    web::Json(Response {
+        result: "success".to_string(),
+    })
+}
+
+fn add_to_data(data: &mut Vec<Pathtrace>, trace_data: Box<Trace>) {
+    // TODO: create the pathtrace and append to it
+    let mut pathtrace = Pathtrace::new();
+    pathtrace.push(*trace_data);
+
+    data.pathtraces.push(pathtrace)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .app_data(web::Data::new(List { pathtraces: vec![] }))
+            .service(trace)
+    })
+    .bind(("127.0.0.1", 3030))?
+    .run()
+    .await
+}
---
+++ src/trace.rs
@@ -0,0 +1,84 @@
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Trace {
+    pub id: Option<String>,
+    pub app_key: String,
+    pub span_name: String,
+    pub service: Option<String>,
+    pub attributes: HashMap<String, String>,
+}
+
+#[derive(Debug)]
+pub struct List {
+    pub pathtraces: Vec<Pathtrace>,
+}
+
+#[derive(Debug)]
+pub struct Pathtrace {
+    head: Option<Rc<RefCell<Node>>>,
+}
+
+#[derive(Debug)]
+struct Node {
+    value: Trace,
+    next: Option<Rc<RefCell<Node>>>,
+}
+
+impl Pathtrace {
+    pub fn new() -> Self {
+        Self { head: None }
+    }
+
+    pub fn push(&mut self, value: Trace) {
+        let node = Rc::new(RefCell::new(Node::new(value)));
+
+        match &self.head {
+            Some(head) => head.borrow_mut().append(node),
+            None => self.head = Some(node),
+        }
+    }
+}
+
+impl Node {
+    pub fn new(value: Trace) -> Self {
+        Node { value, next: None }
+    }
+
+    pub fn append(&mut self, value: Rc<RefCell<Node>>) {
+        self.next = Some(value);
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use std::collections::HashMap;
+
+    use super::{Pathtrace, Trace};
+
+    #[test]
+    fn should_create_pathtraces() {
+        let trace = Trace {
+            id: None,
+            app_key: "testing".to_string(),
+            span_name: "test-span".to_string(),
+            service: Some("delta".to_string()),
+            attributes: HashMap::new(),
+        };
+        let trace_delta = Trace {
+            id: None,
+            app_key: "testing".to_string(),
+            span_name: "test-span-delta".to_string(),
+            service: None,
+            attributes: HashMap::new(),
+        };
+        let mut pathtrace = Pathtrace::new();
+        pathtrace.push(trace);
+        pathtrace.push(trace_delta);
+
+        println!("{pathtrace:?}");
+    }
+}
